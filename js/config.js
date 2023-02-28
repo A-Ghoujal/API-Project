93a2cd6ef4d35a7d19eb91c9f8befa9e8d4e091c
Abdulrahman <abdogohjal@gmail.com>
2023-02-28 15:13:10 +0100

    added new currency api commented out the old one and added css style and html code made the code tidy

--- js/config.js
+++ js/config.js
@@ -1,2 +1,15 @@
 const MY_API_KEY = "46f31e004bmsh480ac21cb7972c3p1f2929jsnfabb785c4d1d";
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
 export { MY_API_KEY };
+export {currencyEl_one}
+export {currencyEl_two}
+export {amountEl_one}
+export {amountEl_two}
+export {rateEl}
+export {swap}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 import { MY_API_KEY } from './config.js';
+import {currencyEl_one} from './config.js';
+import {currencyEl_two} from './config.js';
+import {amountEl_one} from './config.js';
+import {amountEl_two} from './config.js';
+import {rateEl} from './config.js';
+import {swap} from './config.js';
+
 
 (function onLoad()
 {
@@ -7,14 +14,14 @@ import { MY_API_KEY } from './config.js';
 
     // fetch from each API when the page loads
     getJoke();
-    getCurrencyExchangeRates();
+    // getCurrencyExchangeRates();
     
 })();
 
 function setButtonFunctions()
 {
     document.getElementById('buttonGetJoke').onclick = getJoke;
-    document.getElementById('buttonCurrency').onclick = getCurrencyExchangeRates;
+    // document.getElementById('buttonCurrency').onclick = getCurrencyExchangeRates;
     };
 
 
@@ -33,7 +40,6 @@ async function getJoke()
     .then(response => {
         console.log("Chuck Norris API object:");
         console.log(response);
-        console.log("\n");
 
         // display data
         document.getElementById('joke').innerHTML = response.value;
@@ -45,28 +51,61 @@ async function getJoke()
 }
 
 // Currency Exchange rates
-async function getCurrencyExchangeRates()
-{
-    const from = document.getElementById('inputCurrencyFrom').value;
-    const to = document.getElementById('inputCurrencyTo').value;
-    await fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + from + "&to=" + to, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
-            "x-rapidapi-key": MY_API_KEY,
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        console.log("Currency Exchange API object:");
-        console.log(response);
-        console.log("\n");
+// async function getCurrencyExchangeRates()
+// {
+//     const from = document.getElementById('inputCurrencyFrom').value;
+//     const to = document.getElementById('inputCurrencyTo').value;
+//     await fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + from + "&to=" + to, {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+//             "x-rapidapi-key": MY_API_KEY,
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(response => {
+//         console.log("Currency Exchange API object:");
+//         console.log(response);
+//         console.log("\n");
 
-        // display data
-        document.getElementById('currencyResult').innerHTML = 'Result: ' + response;
-    })
-    .catch(err => {
-        console.log(err);
-    });
-}
+//         // display data
+//         document.getElementById('currencyResult').innerHTML = 'Result: ' + response;
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+// }
+
+// Xcurrency 
+
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+  
+    fetch(`https://v6.exchangerate-api.com/v6/cd41f482039f5c09441ae9f9/latest/${currency_one}`)
+      .then((res) => res.json())
+      .then((data) => {
+        //   console.log(data);
+        const rate = data.conversion_rates[currency_two];
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+  
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+      });
+  }
+  
+  // Event Listeners
+  currencyEl_one.addEventListener('change', calculate);
+  amountEl_one.addEventListener('input', calculate);
+  currencyEl_two.addEventListener('change', calculate);
+  amountEl_two.addEventListener('input', calculate);
+  swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+  });
+  
+  calculate();
+
+
 
